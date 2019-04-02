23dc44505d11d6d54f585d2431666bf6f70f79e1
HyperIon-code <3.14forpie@gmail.com>
2019-04-02 06:09:51 -0400

    Top Detection Works

--- visionSystemVS2/image_functions.py
+++ visionSystemVS2/image_functions.py
@@ -6,12 +6,47 @@ import cv2
 import numpy as np
 def crop_image(img,minX,minY,maxX,maxY,w):
     cropped= img[minY:maxY, minX:maxX];
-    cropped = imutils.resize(cropped, width=w)
-    cv2.rectangle(img, (minX, minY), (maxX,maxY), (255, 0,0),1,8,0)
-    cv2.putText(img, "Region of Interest", (minX, minY - 10),
-        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+#    cropped = imutils.resize(cropped, width=w)
     return cropped
 
+def processX(image):
+    threshold_diff=50
+    threshold_top=100
+    minX=145
+    minY=70
+    maxX=minX+150
+    maxY=75
+    scale=1
+    resizeX=crop_image(image,minX,minY,maxX,maxY,int((maxX-minX)*scale))
+    cannyX=conv_image(resizeX,threshold_top,threshold_diff)
+    bluredX=cv2.GaussianBlur(cannyX,(5,5),0)
+    lines=cv2.HoughLinesP(bluredX,1,np.pi/180,1,1,1);
+    x=lines_to_x(lines)+minX
+    return x
+#    cv2.rectangle(img, (minX, minY), (maxX,maxY), (255, 0,0),2,8,0)
+#    cv2.putText(img, "Region of Interest X", (minX, minY + 20),
+#        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+def processY(image,x):
+    if(x.size>=2 and (x[1]-x[0])>2):
+        x_mid=(x[1]-x[0])/2+x[0]
+        threshold_diff=10
+        threshold_top=100
+        minX=int(x_mid-6)
+        minY=70
+        maxX=int(x_mid+6)
+        maxY=500
+        scale=1
+        resizeY=crop_image(image,minX,minY,maxX,maxY,int((maxX-minX)*scale))
+        cannyY=conv_image(resizeY,threshold_top,threshold_diff)
+        bluredY=cv2.GaussianBlur(cannyY,(5,5),0)
+        lines=cv2.HoughLinesP(bluredY,rho=1,theta=np.pi/180,threshold=1,minLineLength=5,maxLineGap=5)
+        y=lines_to_y(lines)+minY
+        cv2.imshow("Canny", cannyY)
+        y=np.array([y[0],y[0]+95])
+        y=y+30
+        return y
+    
+    
 def conv_image(img,threshold_top,threshold_diff):    
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     canny=cv2.Canny(gray, threshold1=threshold_top-threshold_diff, threshold2=threshold_top)
@@ -39,13 +74,31 @@ def lines_to_x(lines):
         return processedX
     else:
         return np.array([150,150+55])
-                    
-def draw_pipes(img, walls):
-    if(walls.size>=2):
-        cv2.rectangle(img, (int(walls[0]), 60), (int(walls[1]),185), (255, 0,0),1,8,0)
-        #cv2.rectangle(img, (int(walls[2]), 520), (int(walls[3]),375), (255, 0,0),1,8,0)
 
-def find_gap(lines):
-    #for line in lines:
-    rect=0;   
-    return rect
+def lines_to_y(lines):
+    if not(lines is None):
+        rawY=np.array([])
+        processedY=np.array([])
+        for line in lines:
+            coords=line[0]
+            if(np.abs(np.arctan((coords[3]-coords[1])/(coords[2]-coords[0])))<np.pi/2.5 or 1==1):
+                    rawY=np.append(rawY,coords[3])
+        rawY=np.sort(rawY, axis=None)
+        last_coord=-50
+        for coord in rawY:
+            if coord>(last_coord+50)and processedY.size<2:
+                processedY=np.append(processedY,coord)
+                last_coord=coord
+        return processedY
+    else:
+        return np.array([250,250+60])
+
+def draw_overlay(image,x,y):
+    cv2.rectangle(image, (65, 60), (395,520), (255, 0,0),1,8,0)
+    draw_pipes(image, x,y)
+
+def draw_pipes(img, x, y):
+    if(x.size>=2 and y.size>=1):
+        cv2.rectangle(img, (int(x[0]), 60), (int(x[1]),int(y[0])), (0, 255,00),2,8,0)
+        cv2.rectangle(img, (int(x[0]), 520), (int(x[1]),int(y[1])), (0, 255,0),2,8,0)
+
--- visionSystemVS2/outline_pipes.py
+++ visionSystemVS2/outline_pipes.py
@@ -8,27 +8,14 @@ import numpy as np
 camera,rawCapture=initialize_camera()
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     last_time=time.time()
-    threshold_diff=50
-    threshold_top=100
+    
     image = frame.array
-    cv2.rectangle(image, (50, 60), (int(walls[1]),185), (255, 0,0),1,8,0)
-    minX=145
-    minY=70
-    maxX=295
-    maxY=75
-
-    scale=1
-    resize=crop_image(image,minX,minY,maxX,maxY,int((maxX-minX)*scale))
-    canny=conv_image(resize,threshold_top,threshold_diff)
-    blured=cv2.GaussianBlur(canny,(5,5),0)
-    lines=cv2.HoughLinesP(blured,1,np.pi/180,1,1,1);
-    x=lines_to_x(lines)+minX
-    resize=imutils.resize(resize, width=400)
-    print(x)
-    draw_pipes(image, x)
+    original=image
+    x=processX(image)
+    y=processY(original,x)
+    print(y)
+    draw_overlay(image,x,y);
     cv2.imshow("Image", image)
-    canny=imutils.resize(canny, width=600)
-    cv2.imshow("Canny", canny)
     key = cv2.waitKey(1) & 0xFF
     rawCapture.truncate(0)
     print("FPS: "+str(1/(time.time()-last_time)))
